da5d8c5b803d856179b2690c97f33861ad7a9d27
- <-@DESKTOP-5LUH4K3>
2023-09-08 13:14:29 +0300

    Done

--- IntroductionToOOP/String/Source.cpp
+++ IntroductionToOOP/String/Source.cpp
@@ -35,19 +35,19 @@ public:
 		//this->str = new char[size] {};
 		std::cout << "DefaultConstruct:" << this << std::endl;
 	}
-	String(const char* str): size(strlen(str)+1),str(new char[size]{})
+	String(const char* str): String(strlen(str)+1)
 	{
 		//this->size = strlen(str) + 1;
 		//this->str = new char[size] {};
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		std::cout << "Constructor:\t" << this << std::endl;
 	}
-	String(const String& other):size(other.size),str(new char[size]{})
+	String(const String& other):String(other.str)
 	{
 		//Deep copy
 		//this->size = other.size;
 		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		std::cout << "CopyConstructor:" << this << std::endl;
 	}
 	String(String&& other):size(other.size),str(other.str)
